e865a8e8d2cc24b669545c4562b1ae9150dc8080
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-03-23 18:52:27 -0300

    add class ListCities and example

---
+++ function/ListCities.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListCities {
+    public List<String> getCities() {
+        List<String> cities = new ArrayList<>();
+        cities.add("Argentina");
+        cities.add("Fracia");
+        cities.add("Croacia");
+        cities.add("Marruecos");
+        cities.add("Brazil");
+        cities.add("Inglaterra");
+        cities.add("Portugal");
+        return cities;
+    }
+}
